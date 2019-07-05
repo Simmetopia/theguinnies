2b228b6a9e2e053d50fe665517d00b1ccce58dbe
Simon Bundgaard-Egeberg <egeberg.simon@gmail.com>
2019-07-05 11:05:29 +0200

    herrl

--- apollo.config.js
+++ apollo.config.js
@@ -2,7 +2,6 @@ module.exports = {
   client: {
     tagName: 'graphql',
     includes: ['./.cache/**/*.js', './src/**/*.{ts,tsx}'], // array of glob patterns
-    service: { name: 'theGuinnies', url: 'http://localhost:8000/__graphql' },
+    service: { name: 'theGuinnies', url: 'http://192.168.5.130:8000/___graphql' },
   },
-  engine: null,
 };
